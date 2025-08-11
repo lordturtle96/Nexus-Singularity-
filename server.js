007cecb5d5903b153ac434b7c9e1d65fde476209
George T <turtlegeorge1996@gmail.com>
2025-08-11 05:11:24 +0100

    Create server.js
    Signed-off-by: George T <turtlegeorge1996@gmail.com>

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+// Example API route
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Welcome to the Nexus Singularity API!' });
+});
+
+// Serve the main HTML file for all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
